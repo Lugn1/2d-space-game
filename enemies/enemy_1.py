503c66180c2bb605ca50b55057ec33c0f99016ea
William Karlström <wkarlstrom@outlook.com>
2023-10-06 01:27:37 +0200

    Add enemy that shoots

---
+++ enemies/enemy_1.py
@@ -0,0 +1,33 @@
+import pygame
+
+ENEMY_WIDTH = 45
+ENEMY_HEIGHT = 30 
+PROJECTILE_WIDTH = 6
+PROJECTILE_HEIGHT = 12
+PROJECTILE_VELOCITY = 3
+projectiles = []
+ENEMY_VELOCITY = 2
+
+class Enemy:
+    def __init__(self, x, y, img, projectiles, velocity):
+        self.x = x
+        self.y = y
+        self.img = img
+        self.shoot_count = 0
+        self.projectiles = projectiles
+        self.velocity = velocity
+        self.rect = pygame.Rect(self.x, self.y, ENEMY_WIDTH, ENEMY_HEIGHT)
+
+    def move(self):
+        self.y += self.velocity
+        self.rect.y = self.y
+
+    def shoot(self):
+        if self.shoot_count == 60:
+            projectile_x = self.x + ENEMY_WIDTH / 2 - PROJECTILE_WIDTH / 2
+            projectile_y = self.y + 20
+            projectile = pygame.Rect(projectile_x, projectile_y, PROJECTILE_WIDTH, PROJECTILE_HEIGHT)
+            self.projectiles.append(projectile)
+            self.shoot_count = 0
+        self.shoot_count += 1    
+            
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 import time
 import random 
+from enemies.enemy_1 import Enemy
+
 pygame.font.init()
 
 WIDTH, HEIGHT = 1000, 800
@@ -12,10 +14,13 @@ BULLET_WIDTH = 6
 BULLET_HEIGHT = 12
 bullets = []
 
-
+ENEMY_VELOCITY = 2
+ENEMY1_WIDTH = 45
+ENEMY1_HEIGHT = 30 
+enemies = []
 PROJECTILE_WIDTH = 6
 PROJECTILE_HEIGHT = 12
-PROJECTILE_VELOCITY = 3
+PROJECTILE_VELOCITY = 4
 projectiles = []
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -28,6 +33,10 @@ try:
     BG = pygame.image.load("./img/lvl1bg.jpg")
     BG = pygame.transform.scale(BG, (WIDTH, HEIGHT))
     playerShip = pygame.image.load("./sprites/playerShip.png")
+    enemy1 = pygame.image.load("./sprites/enemy1.png")
+    enemy1 = pygame.transform.scale(enemy1, (ENEMY1_WIDTH, ENEMY1_HEIGHT))
+    print("enemy1 transformed")
+    
 except Exception as e:
     print("Error loading image", e)
 
@@ -35,7 +44,7 @@ def shoot(x, y):
     bullet = pygame.Rect(x, y - BULLET_HEIGHT, BULLET_WIDTH, BULLET_HEIGHT)
     bullets.append(bullet)
 
-def draw(player, elapsed_time, projectiles):
+def draw(player, elapsed_time, projectiles, enemies):
     WIN.blit(BG, (0, 0))
     
     time_text = FONT.render(F"Time: {round(elapsed_time)}", 1, "white")
@@ -43,7 +52,12 @@ def draw(player, elapsed_time, projectiles):
 
     WIN.blit(playerShip, (player.x, player.y))
 
-    pygame.draw.rect(WIN, "red", player, 2)
+    #pygame.draw.rect(WIN, "red", player, 2)
+    
+
+
+    for enemy in enemies:
+        WIN.blit(enemy1, (enemy.x, enemy.y))
     
     for bullet in bullets:
         pygame.draw.rect(WIN, "green", bullet)
@@ -59,6 +73,8 @@ def main():
     run = True
 
     player = pygame.Rect(WIDTH/2 - 40, HEIGHT/2, PLAYER_WIDTH, PLAYER_HEIGHT)
+    
+    
 
     clock = pygame.time.Clock()
     start_time = time.time()
@@ -67,22 +83,52 @@ def main():
     projectile_add_increment = 2000
     projectile_count = 0
 
+    enemy_spawn_increment = 1500
+    enemy_count = 0
+    
+
     hit = False
     spacebar_held = False
     can_shoot = True
 
     while run:
-        projectile_count += clock.tick(60)
+        enemy_count += clock.tick(60)
         elapsed_time = time.time() - start_time
 
-        if projectile_count > projectile_add_increment:
-            for _ in range(3):
-                projectile_x = random.randint(0, WIDTH - PROJECTILE_WIDTH)
-                projectile = pygame.Rect(projectile_x, -PROJECTILE_HEIGHT, PROJECTILE_WIDTH, PROJECTILE_HEIGHT)
-                projectiles.append(projectile)
+        time_thresholds = [
+            (10, 1400),
+            (20, 1200),
+            (30, 900),
+            (40, 600),
+            (50, 300),
+        ]
 
-            projectile_add_increment = max(200, projectile_add_increment - 50)
-            projectile_count = 0
+        if enemy_count > enemy_spawn_increment:
+            enemy_x_position = random.randint(0, WIDTH - PLAYER_WIDTH) 
+            enemy = Enemy(enemy_x_position, -50, enemy1, projectiles, ENEMY_VELOCITY)
+            enemies.append(enemy)
+            enemy_count = 0
+            for time_threshold, spawn_increment in time_thresholds:
+                if elapsed_time >= time_threshold:
+                    enemy_spawn_increment = spawn_increment
+                else:
+                    break
+
+            if elapsed_time >= 60:        
+                win_text = FONT.render("YOU WON!", 1, "white")
+                WIN.blit(win_text, (WIDTH/2 - win_text.get_width()/2, HEIGHT/2 - win_text.get_height()/2))
+                pygame.display.update()
+                pygame.time.delay(4000)     
+                break       
+                            # TODO: Make the game continue until all enemies have left the screen then win_text
+                            
+            
+               
+
+        for enemy in enemies:
+            enemy.move()
+            enemy.shoot()
+ 
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -109,8 +155,13 @@ def main():
 
         for bullet in bullets[:]:
             bullet.y -= BULLET_VELOCITY
-            if bullet.y < 0:
-                bullets.remove(bullet)
+
+        for bullet in bullets[:]:    
+            for enemy in enemies[:]:
+                if bullet.colliderect(enemy.rect):
+                    enemies.remove(enemy)
+                    bullets.remove(bullet)
+                    break
 
         for projectile in projectiles[:]:
             projectile.y += PROJECTILE_VELOCITY
@@ -128,7 +179,7 @@ def main():
             pygame.time.delay(4000)
             break
 
-        draw(player, elapsed_time, projectiles)    
+        draw(player, elapsed_time, projectiles, enemies)    
 
     pygame.quit()
 
